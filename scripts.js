e13848ff5f0a27e1a6de8760fdd71886cad23fcf
Aptech-stars <tssclassaptech@gmail.com>
2024-10-15 14:49:24 +0500

    Add files via upload

---
+++ scripts.js
@@ -0,0 +1,21 @@
+<script>
+function changeImage(state, image) {
+    if (state == 1) {
+        image.src = "images e shop/silk1 hover.webp"; // Update path if necessary
+    } else if (state == 2) {
+        image.src = "images e shop/silk1.webp"; // Update path if necessary
+    } else if (state == 3) {
+        image.src = "images e shop/silk2 hover.webp"; // Update path if necessary
+    } else if (state == 4) {
+        image.src = "images e shop/silk2.webp"; // Update path if necessary
+    }else if (state == 5) {
+        image.src = "images e shop/silk3 hover.webp"; // Update path if necessary
+    } else if (state == 6) {
+        image.src = "images e shop/silk3.webp"; // Update path if necessary
+    }else if (state == 7) {
+        image.src = "images e shop/silk4 hover.webp"; // Update path if necessary
+    } else if (state == 8) {
+        image.src = "images e shop/silk4.webp"; // Update path if necessary
+    }
+}
+ </script>
